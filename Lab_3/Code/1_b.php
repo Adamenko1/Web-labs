a23d2f306eceb30d6f1903e8ad7ef1a5ec5fb166
Adamenko1 <adamenko2003@gmail.com>
2023-05-27 01:16:25 +0200

    Lab

--- Lab_3/Code/1_b.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo preg_replace_callback('#(\d+)#', 'cube', 'a1b2c3');
-function cube($matches)
-{
-    $result = pow($matches[0], 3); //$matches[0] - это найденное число
-    return $result;
-}
